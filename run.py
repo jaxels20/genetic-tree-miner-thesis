05bb303e8821a840fd87a7c413b810675b8e434f
Kowalski2332 <frederik_kowalski@hotmail.com>
2025-02-26 16:13:40 +0100

    idk anymore

--- run.py
+++ run.py
@@ -8,11 +8,23 @@ import time
 
 
 if __name__ == "__main__":
+    
+    # import cProfile
+    # import pstats
+    
     eventlog = EventLog.from_trace_list(["ABBBC"])
     mutator = Mutator(eventlog, random_creation_rate=0.1, crossover_rate=0.2, mutation_rate=0.5, elite_rate=0.2)
     ga = GeneticAlgorithm(mutator, min_fitness=None, max_generations=200, stagnation_limit=None, time_limit=90, population_size=100)
     start = time.time()
     best_tree = ga.run(eventlog=eventlog)
+    
+    
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+    
+    # profiler.disable()
+    # stats = pstats.Stats(profiler)
+    # stats.strip_dirs().sort_stats("cumulative").print_stats(10)
     
     # Print results
     print(f"Time taken: {time.time() - start}")
