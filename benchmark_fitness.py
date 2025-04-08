6561e240b63d691682d79184ae6b49799b115de9
Jeppe Berg Axlesen <jaxels20@student.aau.dk>
2025-04-08 20:31:02 +0200

    wip

--- benchmark_fitness.py
+++ benchmark_fitness.py
@@ -246,7 +246,7 @@ def real_life_evaluation():
                 our_event_log.traces.sort(key=lambda x: len(x.events), reverse=True)
                 
                 data[filename] = {"FastTokenBasedReplay (without caching)": time_fast_token_based_replay_without_caching(our_event_log, our_net),
-                                "pm4py": time_pm4py_token_based_replay(our_event_log, our_net),
+                                #"pm4py": time_pm4py_token_based_replay(our_event_log, our_net),
                                 "FastTokenBasedReplay (with prefix caching)": time_fast_token_based_replay_with_prefix_caching(our_event_log, our_net),
                                 "FastTokenBasedReplay (with suffix caching)": time_fast_token_based_replay_with_suffix_caching(our_event_log, our_net),
                                 "FastTokenBasedReplay (with prefix and suffix caching)" : time_fast_token_based_replay_with_prefix_and_suffix_caching(our_event_log, our_net),
