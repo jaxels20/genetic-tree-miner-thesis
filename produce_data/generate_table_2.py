e57d1f9df054b1c2f0e53116fcd0f958730632dc
Jeppe Berg Axlesen <jaxels20@student.aau.dk>
2025-05-20 14:49:02 +0200

    Clean up

---
+++ produce_data/generate_table_2.py
@@ -0,0 +1,90 @@
+import csv
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import pandas as pd
+import time
+from src.Discovery import Discovery
+from src.EventLog import EventLog
+from src.Evaluator import SingleEvaluator
+from src.utils import load_hyperparameters_from_csv
+from src.FileLoader import FileLoader
+
+# Data parameters 
+DATASET_DIR = "./real_life_datasets/"
+
+# Genetic Miner Configuration
+BEST_PARAMS = "./best_parameters.csv"
+TIME_LIMIT = 60*5
+STAGNATION_LIMIT = 50
+PERCENTAGE_OF_LOG = 0.05
+OBJECTIVE = {
+    "simplicity": 10,
+    "refined_simplicity": 10,
+    "ftr_precision": 30,
+    "ftr_fitness": 50,
+}
+
+NUM_DATA_POINTS = 5
+OUTPUT_DIR = "./data/table_2/"
+
+
+def generate_data(method: callable, runs: int):    
+    datasets = os.listdir(DATASET_DIR)
+
+    for dataset in datasets:
+        eventlog = FileLoader.load_eventlog(f"{DATASET_DIR}/{dataset}")
+
+        data = []
+        for i in range(runs):
+            print(f"Running discovery on dataset: {dataset} iteration: {i}")
+            start = time.time()
+            discovered_net = method(eventlog)
+            time_taken = time.time() - start
+            
+            os.makedirs(f"{OUTPUT_DIR}/models/GTM", exist_ok=True)
+            discovered_net.to_pnml(f"{OUTPUT_DIR}/models/GTM/{dataset}_{i}")
+            
+            evaluator = SingleEvaluator(
+                discovered_net,
+                eventlog
+            )
+            
+            # Get the evaluation metrics
+            fitness = evaluator.get_replay_fitness()['log_fitness']
+            precision = evaluator.get_precision()
+            
+            metrics = {}
+            metrics['Dataset'] = dataset.split(".")[0]
+            metrics['Discovery Method'] = "GM"
+            metrics['Model'] = i
+            metrics['Log Fitness'] = fitness
+            metrics['Precision'] = precision
+            metrics['F1 Score'] = evaluator.get_f1_score(precision, fitness)
+            metrics['Objective Fitness'] = evaluator.get_objective_fitness(OBJECTIVE)
+            metrics['Generalization'] = evaluator.get_generalization()
+            metrics['Simplicity'] = evaluator.get_simplicity()
+            metrics['Time (s)'] = time_taken
+            data.append(metrics)
+            
+        df = pd.DataFrame(data)
+        df.to_csv(f"{OUTPUT_DIR}/evaluation_results/results_GTM.csv", index=False)
+    
+if __name__ == "__main__":
+    # convert the hyper parameters to a normalize
+    hyper_parameters = load_hyperparameters_from_csv(BEST_PARAMS)
+    
+    # Define model
+    genetic_miner = lambda log: Discovery.genetic_algorithm(
+        log,
+        time_limit=TIME_LIMIT,
+        stagnation_limit=STAGNATION_LIMIT,
+        percentage_of_log=PERCENTAGE_OF_LOG,
+        **hyper_parameters,
+    )
+    # Generate df with results over runs and write to csv
+    generate_data(
+        method = genetic_miner,
+        runs=NUM_DATA_POINTS,
+    )
+
--- produce_figures/generate_table_2.py
+++ produce_figures/generate_table_2.py
@@ -1,9 +1,55 @@
+import pandas as pd
+import os
 
+INPUT_DIR = "./data/table_2/evaluation_results/"
+OUTPUT_FILE = "./figures/table_2.csv"
 
+def consolidate_results(GTM_df):
+    # list all csv files in a directory
+    csv_files = [f for f in os.listdir(INPUT_DIR) if f.endswith('.csv') and f != "results_GTM.csv"]
 
-
-def generate_split_miner_df():
-    """
-    Generates a DataFrame for Split Miner results.
-    """
+    df = GTM_df.copy()
+    for file in csv_files:
+        loaded_df = pd.read_csv(INPUT_DIR + file)
+        df = pd.concat([df, loaded_df], ignore_index=True)
     
+    # df manipulation
+    column_order = ['Dataset', 'Discovery Method', 'F1 Score', 'Log Fitness', 'Precision', 'Generalization', 'Simplicity', 'Objective Fitness', 'Time (s)']
+    df = df[column_order]
+    df.sort_values(by=['Dataset', 'Discovery Method'], inplace=True)
+    df['Time (s)'] = df['Time (s)'].replace('-', 0)
+    
+    agg_df = df.copy()
+    agg_df = agg_df.groupby('Discovery Method').agg({
+        'F1 Score': 'mean',
+        'Log Fitness': 'mean',
+        'Precision': 'mean',
+        'Objective Fitness': 'mean',
+        'Generalization': 'mean',
+        'Simplicity': 'mean',
+        'Time (s)': 'mean'
+    }).reset_index()
+    agg_df['Dataset'] = 'Aggregated'
+    
+    collected_df = pd.concat([df, agg_df], ignore_index=True)
+    collected_df['Time (s)'] = collected_df['Time (s)'].round(2).astype(str)
+    collected_df.loc[(collected_df['Discovery Method'] == 'SM') & (collected_df['Time (s)'] == '0.0'), 'Time (s)'] = '-'
+    
+    return collected_df
+ 
+def get_genetic_miner_median_run():
+    df = pd.read_csv(INPUT_DIR + "results_GTM.csv")
+    median_idx = (
+        df
+        .groupby('Dataset')['Objective Fitness']
+        .apply(lambda x: (x - x.median()).abs().idxmin())    # idxmin returns the index of the first occurrence of the minimum value
+    )
+    df = df.loc[median_idx].reset_index(drop=True)
+    
+    return df
+    
+if __name__ == "__main__":
+    # Take the median of multiple runs and write to csv
+    GTM_df = get_genetic_miner_median_run()
+    df = consolidate_results(GTM_df)
+    df.to_csv(OUTPUT_FILE, index=False, float_format="%.2f")
