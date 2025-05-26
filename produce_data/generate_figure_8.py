9b413ca10176953ee1da730958cee2f33010f381
Jeppe Berg Axlesen <jaxels20@student.aau.dk>
2025-05-26 14:27:33 +0200

    added figure 8

---
+++ produce_data/generate_figure_8.py
@@ -0,0 +1,77 @@
+
+
+# add the parent directory to the system path
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pandas as pd
+import numpy as np
+import os
+from src.FileLoader import FileLoader
+from src.Discovery import Discovery
+from src.EventLog import EventLog 
+from src.Evaluator import SingleEvaluator
+from src.Objective import Objective
+import plotly.graph_objects as go
+import plotly.express as px
+from src.utils import load_hyperparameters_from_csv
+
+INPUT_DIR = "./logs/"
+DATASET = "2013-cp.xes"
+OUTPUT_DIR = "./data/figure_8/"
+
+TIME_LIMT = 60
+STAGNATION_LIMIT = None
+BEST_PARAMS = "./best_parameters.csv"
+percentage_of_logs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
+OBJECTIVE_WEIGHTS = {
+    "simplicity": 10,
+    "refined_simplicity": 10,
+    "ftr_fitness": 50,
+    "ftr_precision": 30
+}
+
+def produce_data():
+    datasets = os.listdir(INPUT_DIR)
+    loader = FileLoader()
+    eventlogs = []
+    
+    best_hyper_parameters = load_hyperparameters_from_csv(BEST_PARAMS)
+    data = []
+
+    for dataset in datasets:
+        if dataset != "2013-i.xes":
+            continue
+        loaded_log = loader.load_eventlog(f"{INPUT_DIR}/{dataset}")
+        eventlogs.append(loaded_log)
+    
+    for eventlog in eventlogs:
+        for percentage_of_log in percentage_of_logs:
+            discovered_net = Discovery.genetic_algorithm(
+                eventlog,
+                time_limit=TIME_LIMT,
+                **best_hyper_parameters,
+                stagnation_limit=STAGNATION_LIMIT,
+                percentage_of_log=percentage_of_log,
+            )
+            
+            evaluator = SingleEvaluator(
+                discovered_net,
+                eventlog
+            )
+            
+            curr_data = {}
+            # Get the evaluation metrics
+            curr_data['objective_fitness'] = evaluator.get_objective_fitness(OBJECTIVE_WEIGHTS) / 100
+            curr_data['dataset'] = eventlog.name
+            curr_data['percentage_of_log'] = percentage_of_log
+            data.append(curr_data)
+        
+    df = pd.DataFrame(data)
+    # check if the output directory exists, if not create it
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    df.to_csv(OUTPUT_DIR + "data.csv", index=False)
+    
+if __name__ == "__main__":
+    produce_data()
